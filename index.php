35fdc3b9f1b50e35166e624d3b58767eadcd4a57
Craig Govender <craig.govender@rank.com>
2022-01-08 11:55:33 +0200

    Component Skeletons

--- index.php
+++ index.php
@@ -1,25 +1,98 @@
-<html>
+<html class="in-progress">
 <head>
 	<link rel="stylesheet" href="dist/styles/main.css"/>
 </head>
 <body>
-	<header>
-		MAIN HEADER
+	<header id="header" class="component header row container">
+		<div class="component image">
+            LOGO
+        </div>
+        <div class="component menu row">
+            <div>MENU ITEM</div>
+            <div>MENU ITEM</div>
+            <div>MENU ITEM</div>
+            <div>MENU ITEM</div>
+            <div>BUTTON</div>
+        </div>
 	</header>
-	<section>
-		SECTION HERO
+	<section id="section-hero" class="container">
+        <div class="component subtitle">
+            ACCENT SUBTITLE
+        </div>
+        <div class="component title">
+            TITLE
+        </div>
+        <div class="row">
+            <div class="component button">
+                BUTTON
+            </div>
+            <div class="component button">
+                BUTTON
+            </div>
+        </div>
 	</section>
-	<section>
-		SECTION COURSES
+	<section id="section-courses" class="container">
+        <div class="row">
+            <div class="component card">
+                <div>IMAGE</div>
+                <div>TITLE</div>
+                <div>TITLE ACCENT</div>
+            </div>
+            <div class="component card">
+                <div>IMAGE</div>
+                <div>TITLE</div>
+                <div>TITLE ACCENT</div>
+            </div>
+            <div class="component card">
+                <div>IMAGE</div>
+                <div>TITLE</div>
+                <div>TITLE ACCENT</div>
+            </div>
+        </div>
+        <div class="component testimonial">
+            <div>BLOCKQUOTE</div>
+            <div>IMAGE</div>
+            <div>NAME</div>
+            <div>LOCATION</div>
+        </div>
 	</section>
-	<section>
-		SECTION PLAYLISTS
+	<section id="section-playlists" class="container">
+        <div class="component">
+            SECTION TITLE
+        </div>
+        <div class="row">
+            <div class="component playlist">
+                <div>IMAGE</div>
+                <div>TITLE</div>
+                <div>EQUIPMENT NEEDED</div>
+            </div>
+            <div class="component playlist">
+                <div>IMAGE</div>
+                <div>TITLE</div>
+                <div>EQUIPMENT NEEDED</div>
+            </div>
+            <div class="component playlist">
+                <div>IMAGE</div>
+                <div>TITLE</div>
+                <div>EQUIPMENT NEEDED</div>
+            </div>
+        </div>
 	</section>
-	<section>
-		SECTION GETTING STARTED
+	<section id="section-getting-started" class="container">
+        <div class="component title accent">
+            TITLE ACCENT
+        </div>
+        <div class="component title">
+            TITLE
+        </div>
+        <div class="component button">
+            BUTTON
+        </div>
 	</section>
-	<footer>
-		SECTION FOOTER
+	<footer class="container">
+		<div class="component copyright">
+            COPYRIGHT
+        </div>
 	</footer>
 </body>
 </html>

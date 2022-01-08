485ab82db569b9758bf0957c3c1c6179d15790eb
Craig Govender <craig.govender@rank.com>
2022-01-08 11:12:22 +0200

    Site Skeleton and SASS Added

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<link rel="stylesheet" href="dist/styles/main.css"/>
+</head>
+<body>
+	<header>
+		MAIN HEADER
+	</header>
+	<section>
+		SECTION HERO
+	</section>
+	<section>
+		SECTION COURSES
+	</section>
+	<section>
+		SECTION PLAYLISTS
+	</section>
+	<section>
+		SECTION GETTING STARTED
+	</section>
+	<footer>
+		SECTION FOOTER
+	</footer>
+</body>
+</html>
